00a6bca3d5d11e1393d2c8a7f646fcf5e833a004
Joao Mendonca <joao.afonso.mtm@gmail.com>
2024-09-30 15:06:29 +0200

    wip: YCBInEOAT dataset

--- dataset/inference_datasets.py
+++ dataset/inference_datasets.py
@@ -9,6 +9,8 @@ import mediapy as media
 from pytorch3d import io as py3d_io
 from pytorch3d import ops as py3d_ops
 from pytorch3d import transforms as py3d_transform
+import trimesh
+import yaml
 
 PROJ_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(PROJ_ROOT)
@@ -39,6 +41,14 @@ LINEMOID_OBJECTS = {
     "lamp": "lamp", 
     "phone": "phone",
     }
+
+YCBINEOAT_OBJECTS = {
+    "mustard_bottle": "mustard_bottle",
+    "bleach_cleanser": "bleach_cleanser",
+    "cracker_box": "cracker_box",
+    "sugar_box": "sugar_box",
+    "tomato_soup_can": "tomato_soup_can",
+}
 
 YCBV_OBJECTS = {
     '002_master_chef_can': 1,
@@ -149,13 +159,14 @@ class LINEMOD_Dataset_GEN6D(torch.utils.data.Dataset):
                     use_binarized_mask=False,
                     num_refer_views=-1, # default -1 means all views
                     num_grid_points=4096):
-        self.name2classID = {'ape': 1, "benchvise": 2,  
-                            'cam': 4, "can": 5,
-                            "cat": 6, "driller": 8, 
-                            "duck": 9, "eggbox": 10, 
-                            "glue": 11, "holepuncher": 12, 
-                            "iron": 13, "lamp": 14, "phone": 15,
-              }
+        # Hacky adjustment to allow the GS-Pose dataset architecture to work with
+        self.benchmark2objname = {
+            "mustard0" : "mustard_bottle",
+        }
+        assert obj_name in self.benchmark2objname.keys(), f"{obj_name} is not in the benchmark2objname"
+        self.benchmark_name = obj_name
+        obj_name = self.benchmark2objname[obj_name]
+        # ---
         assert(subset_mode in ['train', 'test']), f'{subset_mode} is not a valid subset mode [train, test]'
         assert(obj_name in LINEMOID_OBJECTS.keys()), f'{obj_name} is not in the LINEMOD {LINEMOID_OBJECTS}'
         self.subset_mode = subset_mode
@@ -174,17 +185,15 @@ class LINEMOD_Dataset_GEN6D(torch.utils.data.Dataset):
 
         self.to_meter_scale = 1e-3
 
-        self.is_symmetric = False
-        if self.obj_name in ['eggbox', 'glue']:
-            self.is_symmetric = True
+        self.is_symmetric = False # TODO: Check if safe to delete
 
         self.camK = np.array([
             [572.4114 ,   0.     , 325.2611 ],
             [  0.     , 573.57043, 242.049  ],
-            [  0.     ,   0.     ,   1.     ]], dtype=np.float32)
+            [  0.     ,   0.     ,   1.     ]], dtype=np.float32) # TODO: Add camera intrinsics
         self.diameter = gs_utils.read_numpy_data_from_txt(os.path.join(self.obj_dir, 'distance.txt')).squeeze() / 100.0 # in meter
         self.obj_ply_path = os.path.join(self.obj_dir, f'{self.obj_name}.ply') 
-        self.obj_pointcloud = py3d_io.load_ply(self.obj_ply_path)[0].numpy()
+        self.obj_pointcloud = py3d_io.load_ply(self.obj_ply_path)[0].numpy() # TODO: 
 
         self.poses = list()
         self.image_IDs = list()
@@ -330,6 +339,222 @@ class LINEMOD_Dataset_GEN6D(torch.utils.data.Dataset):
 
         return new_batch
 
+class YCBInEOAT_Dataset(torch.utils.data.Dataset):
+    def get_size_from_ply(self):
+        mesh = trimesh.load(self.obj_ply_path)
+        
+        bounding_box = mesh.bounds
+        
+        width = bounding_box[1][0] - bounding_box[0][0]
+        height = bounding_box[1][1] - bounding_box[0][1]
+        depth = bounding_box[1][2] - bounding_box[0][2]
+        
+        width_gt = self.model_info['object_width']
+        ratio = (width_gt / width) * self.to_meter_scale
+        
+        width *= ratio
+        height *= ratio
+        depth *= ratio
+        
+        return width, height, depth
+    def __init__(self, data_root, obj_name, 
+                 subset_mode='train', 
+                 obj_database_dir=None, 
+                 load_gt_bbox=False, 
+                 use_gt_mask=False,
+                 use_binarized_mask=False,
+                 num_refer_views=-1, # default -1 means all views
+                 load_yolo_det=False, 
+                 num_grid_points=4096,
+                 ):
+        # Hacky adjustment to allow the GS-Pose dataset architecture to work with
+        self.benchmark2objname = {
+            "mustard0" : "mustard_bottle",
+        }
+        assert obj_name in self.benchmark2objname.keys(), f"{obj_name} is not in the benchmark2objname"
+        self.benchmark_name = obj_name
+        obj_name = self.benchmark2objname[obj_name]
+        # ---
+
+        assert(subset_mode in ['train', 'test']), f'{subset_mode} is not a valid subset mode [train, test]'
+        assert(obj_name in YCBINEOAT_OBJECTS.keys()), f'{obj_name} is not in the YCBINEOAT {YCBINEOAT_OBJECTS}'
+        self.obj_name = obj_name
+        self.data_root = data_root
+        self.subset_mode = subset_mode
+        self.num_grid_points = num_grid_points
+        self.benchmark_dir = os.path.join(self.data_root, self.benchmark_name)
+        self.obj_dir = os.path.join(self.data_root, 'train', self.obj_name)
+
+        self.use_gt_mask = use_gt_mask
+        self.load_gt_bbox = load_gt_bbox
+        self.load_yolo_det = load_yolo_det
+        self.obj_database_dir = obj_database_dir
+        self.use_binarized_mask = use_binarized_mask
+        self.to_meter_scale = 1e-3 # TODO: Check if YCBINEOAT is in mm or m
+        
+        # Read camK from cam_K.txt (each line is a row of the matrix with space separated values)
+        # TODO: camK might be different for training and testing
+        self.camK = gs_utils.read_numpy_data_from_txt(os.path.join(self.benchmark_dir, 'cam_K.txt'))
+        
+        with open(os.path.join(self.obj_dir, 'dataset_info.yml'), 'r') as f:
+            self.model_info = yaml.load(f, Loader=yaml.FullLoader)
+            
+        self.obj_ply_path = os.path.join(self.obj_dir, 'textured.ply')
+        self.obj_pointcloud = py3d_io.load_ply(self.obj_ply_path)[0].numpy() * self.to_meter_scale # convert to m
+
+
+        # self.is_symmetric = False # TODO: Don't think this is needed
+        # for _key, _val in self.obj_model_info.items():
+        #     if 'symmetries' in _key:
+        #         self.is_symmetric = True
+
+        # self.diameter = self.obj_model_info['diameter'] * self.to_meter_scale # convert to m # TODO: No clue what this diameter refers to
+        
+        size_x, size_y, size_z = self.get_size_from_ply()
+        
+        bbox3d_xyz = np.array([size_x,
+                                size_y,
+                                size_z,
+                            ])
+        self.obj_bbox3d = np.array([
+            [-bbox3d_xyz[0], -bbox3d_xyz[0], -bbox3d_xyz[0], -bbox3d_xyz[0],  bbox3d_xyz[0],  bbox3d_xyz[0],  bbox3d_xyz[0],  bbox3d_xyz[0]],
+            [-bbox3d_xyz[1], -bbox3d_xyz[1],  bbox3d_xyz[1],  bbox3d_xyz[1], -bbox3d_xyz[1], -bbox3d_xyz[1],  bbox3d_xyz[1],  bbox3d_xyz[1]],
+            [-bbox3d_xyz[2],  bbox3d_xyz[2],  bbox3d_xyz[2], -bbox3d_xyz[2], -bbox3d_xyz[2],  bbox3d_xyz[2],  bbox3d_xyz[2], -bbox3d_xyz[2]]
+            ]).T / 2 
+        self.bbox3d_diameter = np.linalg.norm(bbox3d_xyz)
+        
+        # yolo_detection_dir = os.path.join(DATASPACE_DIR, 'bop_dataset/lm_yolo_detection/val')   # TODO: YOLO detection
+        # obj_yolo_detect_name = '08{:02d}-lm{}-others'.format(self.obj_classID, self.obj_classID)
+        # obj_yolo_label_dir = os.path.join(yolo_detection_dir, obj_yolo_detect_name, 'labels')
+        
+        self.poses_file = os.path.join(self.obj_dir, 'scene_gt.json')
+        with open(self.poses_file, 'r') as f:
+            self.poses_info = json.load(f)
+
+        if self.load_gt_bbox:
+            self.bboxes_file = os.path.join(self.obj_dir, 'scene_gt_info.json')
+            with open(self.bboxes_file, 'r') as f:
+                self.bboxes_info = json.load(f)
+            self.gt_bboxes = dict()
+
+        self.poses = list()
+        self.image_IDs = list()
+        self.allo_poses = list()
+        self.image_paths = list()
+        self.yolo_bboxes = dict()
+        image_subset_lists = gs_utils.read_list_data_from_txt(os.path.join(self.obj_dir, f'{self.subset_mode}.txt'))
+        for idx, img_inst in enumerate(image_subset_lists):
+            image_ID = int(img_inst)
+            if image_ID >= len(self.poses_info):
+                # TODO: This shouldn't be happening but for some reason some have a bit more, lets log which ones
+                print(f"Image ID {image_ID} is out of bounds for {self.obj_name} in {self.subset_mode} mode")
+                # Add to end of .txt file
+                with open(os.path.join(self.data_root, 'errors.txt'), 'a') as f:
+                    f.write(f"Image ID {image_ID} is out of bounds for {self.obj_name} in {self.subset_mode} mode\n")
+                
+                continue
+            image_path = os.path.join(self.obj_dir, 'rgb', '{:06d}.png'.format(image_ID))
+            pose_RT = self.poses_info[str(image_ID)][0]
+            obj_pose = np.eye(4)
+            obj_pose[:3, :3] = np.array(pose_RT['cam_R_m2c'], dtype=np.float32).reshape(3, 3)
+            obj_pose[:3, 3] = np.array(pose_RT['cam_t_m2c'], dtype=np.float32).reshape(3) * self.to_meter_scale # convert to m
+
+            self.poses.append(obj_pose)
+            self.image_paths.append(image_path)
+
+            allo_pose = obj_pose.copy() 
+            allo_pose[:3, :3] = gs_utils.egocentric_to_allocentric(allo_pose)[:3, :3]
+            self.allo_poses.append(allo_pose)
+
+            self.image_IDs.append(image_ID)
+
+            if self.load_gt_bbox:
+                gt_x1, gt_y1, gt_bw, gt_bh = self.bboxes_info[str(image_ID)][0]['bbox_visib']
+                gt_x2 = gt_x1 + gt_bw
+                gt_y2 = gt_y1 + gt_bh
+                self.gt_bboxes[image_ID] = np.array([gt_x1, gt_y1, gt_x2, gt_y2])
+
+            # if self.load_yolo_det: # TODO: YOLO substitute with SAM detection
+            #     yolo_bbox_path = os.path.join(obj_yolo_label_dir, '{:06d}.txt'.format(image_ID + 1)) # yolo_results starts from 1
+            #     if os.path.exists(yolo_bbox_path):
+            #         yolo_box = np.loadtxt(yolo_bbox_path)
+            #         assert yolo_box.shape[0] != 0, f"img id:{image_ID} no box detected!"
+            #         if len(yolo_box.shape) == 2:
+            #             want_id = np.argsort(yolo_box[:,5])[0]
+            #             yolo_box = yolo_box[want_id]
+            #         x_c_n, y_c_n, w_n, h_n = yolo_box[1:5]
+            #         x0_n, y0_n = x_c_n - w_n / 2, y_c_n - h_n / 2
+            #         x1_n, y1_n = x_c_n + w_n / 2, y_c_n + h_n / 2
+            #         self.yolo_bboxes[image_ID] = np.array([x0_n, y0_n, x1_n, y1_n])
+                # else:
+                #     self.yolo_bboxes[image_ID] = np.array([0, 0, 1.0, 1.0])
+        
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        data_dict = dict()
+        camK = self.camK
+        pose = self.poses[idx]
+        image_ID = self.image_IDs[idx]
+        allo_pose = self.allo_poses[idx]
+        image_path = self.image_paths[idx]
+        image = np.array(Image.open(image_path), dtype=np.uint8) / 255.0
+
+        data_dict['camK'] = torch.as_tensor(camK, dtype=torch.float32) 
+        data_dict['pose'] = torch.as_tensor(pose, dtype=torch.float32)
+        data_dict['image'] = torch.as_tensor(image, dtype=torch.float32)    
+        data_dict['allo_pose'] = torch.as_tensor(allo_pose, dtype=torch.float32)
+
+        data_dict['image_ID'] = image_ID
+        data_dict['image_path'] = image_path
+
+        if self.use_gt_mask:
+            mask_path = os.path.join(self.obj_dir, 'mask_visib', f'{image_ID:06d}_000000.png')
+            data_dict['gt_mask_path'] = mask_path
+        
+        if self.obj_database_dir is not None:
+            data_dict['coseg_mask_path'] = os.path.join(self.obj_database_dir, 'pred_coseg_mask', '{:06d}.png'.format(image_ID))
+
+        if self.load_yolo_det and self.yolo_bboxes.get(image_ID, None) is not None:
+            img_hei, img_wid = image.shape[:2]
+            x0_n, y0_n, x1_n, y1_n = self.yolo_bboxes[image_ID]
+            x0_n, x1_n = x0_n * img_wid, x1_n * img_wid
+            y0_n, y1_n = y0_n * img_hei, y1_n * img_hei
+            
+            bbox_xyxy = np.array([x0_n, y0_n, x1_n, y1_n])
+            data_dict['yolo_bbox'] = torch.as_tensor(bbox_xyxy, dtype=torch.float32)
+
+            bbox_scale = max(x1_n - x0_n, y1_n - y0_n)
+            bbox_center = np.array([(x0_n + x1_n) / 2, (y0_n + y1_n) / 2])
+            data_dict['bbox_scale'] = torch.as_tensor(bbox_scale, dtype=torch.float32)
+            data_dict['bbox_center'] = torch.as_tensor(bbox_center, dtype=torch.float32)
+            
+        if self.load_gt_bbox:
+            x1, y1, x2, y2 = self.gt_bboxes[image_ID]
+            bbox_scale = max(x2 - x1, y2 - y1)
+            bbox_center = np.array([(x1 + x2) / 2, (y1 + y2) / 2])
+            data_dict['gt_bbox_scale'] = torch.as_tensor(bbox_scale, dtype=torch.float32)
+            data_dict['gt_bbox_center'] = torch.as_tensor(bbox_center, dtype=torch.float32)
+
+
+        return data_dict
+
+    def collate_fn(self, batch):
+        """
+        batchify the data
+        """
+        new_batch = dict()
+        for each_dat in batch:
+            for key, val in each_dat.items():
+                if key not in new_batch:
+                    new_batch[key] = list()
+                new_batch[key].append(val)
+
+        for key, val in new_batch.items():
+            new_batch[key] = torch.stack(val, dim=0)
+
+        return new_batch
 
 class LINEMOD_Dataset_BOP(torch.utils.data.Dataset):
     def __init__(self, data_root, obj_name, 
@@ -422,6 +647,14 @@ class LINEMOD_Dataset_BOP(torch.utils.data.Dataset):
         image_subset_lists = gs_utils.read_list_data_from_txt(os.path.join(self.obj_dir, f'{self.subset_mode}.txt'))
         for idx, img_inst in enumerate(image_subset_lists):
             image_ID = int(img_inst)
+            if image_ID >= len(self.poses_info):
+                # TODO: This shouldn't be happening but for some reason some have a bit more, lets log which ones
+                print(f"Image ID {image_ID} is out of bounds for {self.obj_name} in {self.subset_mode} mode")
+                # Add to end of .txt file
+                with open(os.path.join(self.data_root, 'errors.txt'), 'a') as f:
+                    f.write(f"Image ID {image_ID} is out of bounds for {self.obj_name} in {self.subset_mode} mode\n")
+                
+                continue
             image_path = os.path.join(self.obj_dir, 'rgb', '{:06d}.png'.format(image_ID))
             pose_RT = self.poses_info[str(image_ID)][0]
             obj_pose = np.eye(4)
@@ -797,7 +1030,11 @@ datasetCallbacks = {
                         'DATASETLOADER': LOWTEXTUREVideo_Dataset, 
                         'DATAROOT': os.path.join(DATASPACE_DIR, 'onepose_dataset/lowtexture_test_data'),
                         },
-              
+    
+    'YCBINEOAT': {'OBJECTS': YCBINEOAT_OBJECTS,
+                    'DATASETLOADER': YCBInEOAT_Dataset, 
+                    'DATAROOT': os.path.join(DATASPACE_DIR, 'ycbineoat'),
+                },
 }
 
 
--- posingpixels/optimization.py
+++ posingpixels/optimization.py
@@ -75,13 +75,16 @@ def render_train_model(
     model,
     camKs,
     target_points,
-    num_epochs=1000,
-    warmup_steps=10,
+    num_epochs=2000,
+    warmup_steps=100,
     max_lr=5e-3,
     min_lr=0.0,
     early_stop_min_steps=5,
     early_stop_loss_grad_norm=1e-4,
     temporal_consistency_weight=0.1,
+    reconstruction_loss = 'mse',
+    reconstruction_loss_clip=None,
+    reconstruction_loss_clip_start_epoch=200,
     verbose=False,
     return_all=False,
 ):
@@ -92,7 +95,13 @@ def render_train_model(
     lr_scheduler = CosineAnnealingWarmupRestarts(
         optimizer, num_epochs, warmup_steps=warmup_steps, max_lr=max_lr, min_lr=min_lr
     )
-    reconstruction_criterion = nn.MSELoss(reduction="mean")
+    early_stop_min_steps = max(warmup_steps, early_stop_min_steps)
+    if reconstruction_loss == 'mse':
+        reconstruction_criterion = nn.MSELoss(reduction='none')
+    elif reconstruction_loss == 'l1':
+        reconstruction_criterion = nn.L1Loss(reduction='none')
+    else:
+        raise ValueError(f"Unknown reconstruction loss: {reconstruction_loss}")
 
     losses = []
     
@@ -104,6 +113,9 @@ def render_train_model(
         rendered_points = model(camKs)
         
         reconstruction_loss = reconstruction_criterion(rendered_points[:, :, :2], target_points)
+        if reconstruction_loss_clip is not None and epoch > reconstruction_loss_clip_start_epoch:
+            reconstruction_loss = torch.clamp(reconstruction_loss, max=reconstruction_loss_clip)
+        reconstruction_loss = torch.mean(reconstruction_loss)
         consistency_loss = temporal_consistency_loss(model.rotations_6d, model.translation_vectors)
         
         total_loss = reconstruction_loss + temporal_consistency_weight * consistency_loss
@@ -131,123 +143,6 @@ def render_train_model(
 
     with torch.no_grad():
         final_rendered_points = model(camKs)
-        if return_all:
-            rendered_points_history.append(final_rendered_points)
-            rendered_points_history = torch.stack(rendered_points_history, dim=0)
-
-    return model, rendered_points_history if return_all else final_rendered_points
-
-class RenderPoseModel6D(nn.Module):
-    def __init__(self, points, initial_T=None, initial_R=None, device=None):
-        super(RenderPoseModel6D, self).__init__()
-        self.device = device or torch.device(
-            "cuda" if torch.cuda.is_available() else "cpu"
-        )
-        self.to(self.device)
-
-        if initial_R is None:
-            identity_rotation_6d = torch.tensor(
-                [[1.0, 0.0, 0.0, 0.0, 1.0, 0.0]], device=self.device
-            )
-            self.rotation_6d = nn.Parameter(identity_rotation_6d)
-        else:
-            self.rotation_6d = nn.Parameter(
-                matrix_to_rotation_6d(torch.tensor(initial_R, device=self.device))
-            )
-
-        if initial_T is None:
-            self.translation_vector = nn.Parameter(torch.zeros(3, device=self.device))
-        else:
-            self.translation_vector = nn.Parameter(
-                torch.tensor(initial_T, device=self.device)
-            )
-
-        self.canonical_points = nn.Parameter(
-            points.to(self.device), requires_grad=False
-        )
-
-    def transform_points(self, points, rotation_matrix, translation_vector):
-        # Performs: (points @ rotation_matrix.T) + translation_vector (but more efficient)
-        return torch.addmm(translation_vector, points, rotation_matrix.T)
-
-    @staticmethod
-    def render_points(points, camK):
-        return (points @ camK.T) / points[:, 2:3]
-
-    def get_rotation_matrix(self):
-        return rotation_6d_to_matrix(self.rotation_6d).squeeze(0)
-
-    def get_translation_vector(self):
-        return self.translation_vector
-
-    def get_pose_matrix(self):
-        return torch.cat(
-            (self.get_rotation_matrix(), self.get_translation_vector().unsqueeze(0).T),
-            dim=1,
-        )
-
-    def forward(self, camK):
-        rotation_matrix = rotation_6d_to_matrix(self.rotation_6d).squeeze(0)
-        transformed_points = self.transform_points(
-            self.canonical_points, rotation_matrix, self.translation_vector
-        )
-        return self.render_points(transformed_points, camK)
-
-
-def render_train_model_single(
-    model,
-    camK,
-    target_points,
-    num_epochs=400,
-    warmup_steps=10,
-    max_lr=5e-3,
-    min_lr=0.0,
-    early_stop_min_steps=5,
-    early_stop_loss_grad_norm=1e-4,
-    verbose=False,
-    return_all=False,
-):
-    device = model.device
-    camK = camK.to(device)
-    target_points = target_points[:, :2].to(device)
-    # TODO: Different lr for R and T
-    optimizer = optim.Adam(model.parameters())
-    lr_scheduler = CosineAnnealingWarmupRestarts(
-        optimizer, num_epochs, warmup_steps=warmup_steps, max_lr=max_lr, min_lr=min_lr
-    )
-    criterion = nn.MSELoss(reduction="mean")
-    # criterion = nn.L1Loss(reduction="mean")
-
-    losses = []
-
-    if return_all:
-        rendered_points_history = []
-
-    for epoch in range(num_epochs):
-        optimizer.zero_grad()
-        rendered_points = model(camK)
-        loss = criterion(rendered_points[:, :2], target_points)
-        loss.backward()
-        optimizer.step()
-        lr_scheduler.step()
-
-        if return_all:
-            rendered_points_history.append(rendered_points.detach())
-
-        if verbose and epoch % 100 == 0:
-            print(f"Epoch {epoch}: Loss = {loss.item()}")
-
-        losses.append(loss.item())
-        if epoch > early_stop_min_steps:
-            loss_grads = torch.tensor(losses[1:]) - torch.tensor(losses[:-1])
-            loss_grad = loss_grads[-early_stop_min_steps:].abs().mean()
-            if loss_grad < early_stop_loss_grad_norm:
-                if verbose:
-                    print(f"Early stopping at epoch {epoch}")
-                break
-
-    with torch.no_grad():
-        final_rendered_points = model(camK)
         if return_all:
             rendered_points_history.append(final_rendered_points)
             rendered_points_history = torch.stack(rendered_points_history, dim=0)
